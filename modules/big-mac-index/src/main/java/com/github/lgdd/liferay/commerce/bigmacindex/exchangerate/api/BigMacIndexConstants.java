f94c44cba4d3232f91c8c3acfa9e795c0964794b
lgdd <louis-guillaume.durand@liferay.com>
2023-06-03 00:05:54 +0200

    chore: initial commit

---
+++ modules/big-mac-index/src/main/java/com/github/lgdd/liferay/commerce/bigmacindex/exchangerate/api/BigMacIndexConstants.java
@@ -0,0 +1,8 @@
+package com.github.lgdd.liferay.commerce.bigmacindex.exchangerate.api;
+
+public class BigMacIndexConstants {
+
+  public static final String EXCHANGE_RATE_PROVIDER_KEY = "big-mac-index-exchange-rate-provider";
+  public static final String RAW_INDEX_URL = "https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-raw-index.csv";
+
+}
---
+++ modules/big-mac-index/src/main/java/com/github/lgdd/liferay/commerce/bigmacindex/exchangerate/internal/BigMacIndexExchangeRateParser.java
@@ -0,0 +1,38 @@
+package com.github.lgdd.liferay.commerce.bigmacindex.exchangerate.internal;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = BigMacIndexExchangeRateParser.class)
+public class BigMacIndexExchangeRateParser {
+
+  Map<String, BigDecimal> toMap(String csvAsString)
+      throws IOException {
+
+    final Reader rawIndexData = new StringReader(csvAsString);
+    final Iterable<CSVRecord> rawIndexRecords =
+        CSVFormat.Builder.create().setHeader().build().parse(rawIndexData);
+    final int yearDiff = csvAsString.contains(LocalDate.now().getYear() + "-") ? 0 : 1;
+
+    final Map<String, BigDecimal> rawIndexMap = new HashMap<>();
+    StreamSupport.stream(rawIndexRecords.spliterator(), false)
+                 .filter(record -> LocalDate.parse(record.get("date")).getYear()
+                     >= LocalDate.now().getYear() - yearDiff)
+                 .sorted(Comparator.comparing(record -> LocalDate.parse(record.get("date"))))
+                 .forEach(record -> rawIndexMap.put(record.get("currency_code").toUpperCase(),
+                                                    new BigDecimal(record.get("dollar_ex"))));
+
+    return rawIndexMap;
+  }
+
+}
