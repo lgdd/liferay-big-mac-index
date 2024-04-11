4fb7baa61015c52a6de5acd619f10572286dc3d3
lgdd <louis-guillaume.durand@liferay.com>
2024-04-11 16:10:51 +0200

    chore!: update for Liferay 7.4 GA 107

--- modules/big-mac-index/src/main/java/com/github/lgdd/liferay/commerce/bigmacindex/exchangerate/api/BigMacIndexConstants.java
+++ modules/big-mac-index/src/main/java/com/github/lgdd/liferay/commerce/bigmacindex/exchangerate/api/BigMacIndexConstants.java
@@ -2,7 +2,10 @@ package com.github.lgdd.liferay.commerce.bigmacindex.exchangerate.api;
 
 public class BigMacIndexConstants {
 
-  public static final String EXCHANGE_RATE_PROVIDER_KEY = "big-mac-index-exchange-rate-provider";
-  public static final String RAW_INDEX_URL = "https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-raw-index.csv";
+  public static final String EXCHANGE_RATE_PROVIDER_KEY =
+          "Big Mac Index"; // waiting for Liferay to fix the language key for exchange rate provider
+
+  public static final String RAW_INDEX_URL =
+          "https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-raw-index.csv";
 
 }
--- modules/big-mac-index/src/main/java/com/github/lgdd/liferay/commerce/bigmacindex/exchangerate/internal/BigMacIndexExchangeRateProvider.java
+++ modules/big-mac-index/src/main/java/com/github/lgdd/liferay/commerce/bigmacindex/exchangerate/internal/BigMacIndexExchangeRateProvider.java
@@ -7,37 +7,41 @@ import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.util.ExchangeRateProvider;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.StringUtil;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.net.URL;
 import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 @Component(
-    immediate = true,
-    property = "commerce.exchange.provider.key=" + EXCHANGE_RATE_PROVIDER_KEY,
-    service = ExchangeRateProvider.class
+        immediate = true,
+        property = "commerce.exchange.provider.key=" + EXCHANGE_RATE_PROVIDER_KEY,
+        service = ExchangeRateProvider.class
 )
-public class BigMacIndexExchangeRateProvider
-    implements ExchangeRateProvider {
+public class BigMacIndexExchangeRateProvider implements ExchangeRateProvider {
 
   @Override
-  public BigDecimal getExchangeRate(CommerceCurrency primaryCommerceCurrency,
-      CommerceCurrency secondaryCommerceCurrency)
-      throws Exception {
+  public BigDecimal getExchangeRate(
+          CommerceCurrency primaryCommerceCurrency,
+          CommerceCurrency secondaryCommerceCurrency)
+          throws Exception {
 
     final String csv = _http.URLtoString(new URL(RAW_INDEX_URL));
-    final Map<String, BigDecimal> rawIndexMap = _exchangeRateParser.toMap(csv);
+    final Map<String, BigDecimal> rawIndexMap = _exchangeRateParser.toMap(
+            csv);
 
-    final String primaryCurrencyCode =
-        StringUtil.toUpperCase(primaryCommerceCurrency.getCode());
+    final String primaryCurrencyCode = StringUtil.toUpperCase(
+            primaryCommerceCurrency.getCode());
 
-    final String secondaryCurrencyCode =
-        StringUtil.toUpperCase(secondaryCommerceCurrency.getCode());
+    final String secondaryCurrencyCode = StringUtil.toUpperCase(
+            secondaryCommerceCurrency.getCode());
 
     final BigDecimal rateToPrimary = rawIndexMap.get(primaryCurrencyCode);
-    final BigDecimal rateToSecondary = rawIndexMap.get(secondaryCurrencyCode);
+    final BigDecimal rateToSecondary = rawIndexMap.get(
+            secondaryCurrencyCode);
 
     return rateToSecondary.divide(rateToPrimary, 4, RoundingMode.HALF_EVEN);
   }
